52e712e975cf6cab552486243ef95755201a4657
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-11-02 21:27:18 -0600

    add code to handle array serialization

--- src/Main/Serializer.java
+++ src/Main/Serializer.java
@@ -3,18 +3,20 @@ package Main;
 import org.jdom2.Document;
 import org.jdom2.Element;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.InaccessibleObjectException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.IdentityHashMap;
 import java.util.LinkedList;
 
 public class Serializer {
     private static int id = 0;
     // seen keeps track of objects we have already serialized, using its declaring class name
-    private static final HashSet<String> seen = new HashSet<>();
+    private static final HashSet<Object> seen = new HashSet<>();
     // objects maps each object name to its ID
-    private static final HashMap<String, Integer> objects = new HashMap<>();
+    private static final IdentityHashMap<Object, Integer> objects = new IdentityHashMap<>();
     // private static final LinkedList<Object> serializeQueue = new LinkedList<>(); // An option for delaying serialization of referenced objects
 
     // Serialize an object, by creating a base document and attaching results of serializeBody() to it
@@ -43,9 +45,52 @@ public class Serializer {
         root.setAttribute("id", Integer.toString(id));
         id++;
 
+        // Separate case for array
+        if (object.getClass().isArray()) {
+            // Don't bother with fields at all then
+            int length = Array.getLength(object);
+            root.setAttribute("length", String.valueOf(length));
+            for (int i = 0; i < length; i++) {
+                Object arrayElement = Array.get(object, i);
+                if (arrayElement == null) {
+                    Element value = new Element("value");
+                    root.addContent(value);
+                    value.setText("null");
+                    continue;
+                }
+                // Branch depending on whether we have a primitive or an array, or an object
+                if (arrayElement.getClass().isPrimitive()) { // Create an element with "value" tag and add it to field
+                    Element value = new Element("value");
+                    root.addContent(value);
+                    // If primitive, just set value of field to its string representation
+                    value.setText(arrayElement.toString());
+                    continue;
+                }
+                // If an object, recursively serialize it and set value of 'reference' to its id
+                Element reference = new Element("reference");
+                root.addContent(reference);
+
+                // First check if we have already serialized this object
+                if (seen.contains(arrayElement)) {
+                    // Reference our HashMap to get the id of the object
+                    reference.setText(String.valueOf(objects.get(arrayElement)));
+                    continue;
+                }
+                // Recursively serialize the object if we haven't seen it before
+                Document serializedObject = serializeBody(arrayElement);
+                reference.setText(Integer.toString(id));
+                root.addContent(serializedObject.getRootElement());
+            }
+            return doc;
+        }
+
         // Reflectively find fields of object
         Field[] fields = objClass.getDeclaredFields();
         for (Field field : fields) {
+            int modifiers = field.getModifiers();
+            // Skip static fields entirely
+            if (java.lang.reflect.Modifier.isStatic(modifiers)) continue;
+
             // Create an element with "field" tag and add it to object
             Element fieldElement = new Element("field");
             root.addContent(fieldElement);
@@ -61,22 +106,23 @@ public class Serializer {
                     fieldElement.addContent(value);
                     value.setText("null");
                 } else {
-                    // Branch depending on whether we have a primitive or an object
+                    // Branch depending on whether we have a primitive or an array, or an object
                     if (field.getType().isPrimitive()) { // Create an element with "value" tag and add it to field
                         Element value = new Element("value");
                         fieldElement.addContent(value);
                         // If primitive, just set value of field to its string representation
                         value.setText(fieldValue.toString());
+                    } else if (field.getType().isArray()) {
+                        // Run a for loop for length
                     } else {
                         // If an object, recursively serialize it and set value of 'reference' to its id
                         Element reference = new Element("reference");
                         fieldElement.addContent(reference);
-                        String fieldClassName = fieldValue.getClass().getName();
 
                         // First check if we have already serialized this object
-                        if (seen.contains(fieldClassName)) {
+                        if (seen.contains(fieldValue)) {
                             // Reference our HashMap to get the id of the object
-                            reference.setText(String.valueOf(objects.get(fieldClassName)));
+                            reference.setText(String.valueOf(objects.get(fieldValue)));
                             continue;
                         }
                         // Recursively serialize the object if we haven't seen it before
@@ -85,6 +131,7 @@ public class Serializer {
                         root.addContent(serializedObject.getRootElement());
                     }
                 }
+
             } catch (IllegalAccessException | InaccessibleObjectException e) {
                 // Make value of field show that it could not be accessed
                 Element value = new Element("value");
